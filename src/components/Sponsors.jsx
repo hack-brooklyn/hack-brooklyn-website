3007e75719de9208090ea6936737e2cafd3f8f9d
KevinTamCS <kevin@kevintamcs.com>
2021-04-23 18:19:43 -0400

    Add echoAR as a sponsor

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Cisco from "../assets/company-logos/cisco.png";
 import Cuny2x from "../assets/company-logos/cuny-2x.png";
+import echoAr from "../assets/company-logos/echo-ar.png";
 import "../scss/Sponsors.scss";
 
 function Sponsors() {
@@ -14,40 +15,51 @@ function Sponsors() {
           support!
         </p>
 
-        <div className="sponsor-images">
+        <div className="sponsor-container">
           {/* Gold Tier */}
           <div className="sponsor-tier">
             <p className="sponsor-tier-text">Gold Sponsors</p>
-            <Sponsor
-              sponsor="Cisco"
-              tier="gold"
-              logo={Cisco}
-              link="https://www.cisco.com"
-            />
+
+            <div className="sponsor-images">
+              <Sponsor
+                sponsor="Cisco"
+                tier="gold"
+                logo={Cisco}
+                link="https://www.cisco.com"
+              />
+            </div>
           </div>
 
           {/* Bronze Tier and Friends */}
           <div className="sponsor-tier">
             <p className="sponsor-tier-text">Bronze Sponsors</p>
-            <Sponsor
-              sponsor="CUNY 2X"
-              tier="bronze"
-              logo={Cuny2x}
-              link="https://www.techtalentpipeline.nyc/cs-doubling"
-            />
+
+            <div className="sponsor-images">
+              <Sponsor
+                sponsor="CUNY 2X"
+                tier="bronze"
+                logo={Cuny2x}
+                link="https://www.techtalentpipeline.nyc/cs-doubling"
+              />
+
+              <Sponsor
+                sponsor="echoAR"
+                tier="bronze"
+                logo={echoAr}
+                link="https://www.echoar.xyz"
+              />
+            </div>
           </div>
         </div>
 
         <p className="sponsor">
           Care about empowering diverse students to tackle large-scale issues
-          around the world?
-          <br />
-          Get in touch with us to support the cause.
+          around the world? Get in touch with us to support the cause.
         </p>
 
         <a href="mailto:sponsor@hackbrooklyn.org">
           <button type="button" className="btn contact">
-            Contact Us
+            Become a Sponsor
           </button>
         </a>
       </div>
